4c5e0ffe4ba85de8fe963b1c74093f794d239348
MosesOdinaka <iriele.moses@gmail.com>
2023-05-23 03:57:13 +0100

    updated

--- shell.c
+++ shell.c
@@ -9,17 +9,21 @@
 int main(void)
 {
 	char **arg;
-	int looper = 1;
+	int looper = 1, interact = isatty(STDIN_FILENO);
 	char *ln = NULL;
 	size_t n = 0;
 
 	while (looper)
 	{
+		if (interact)
+		{
 		printf("ManShell($) ");
 		fflush(stdout);
-
+		}
 		rd_cmdline(&ln, &n, &arg);
 		cmd_exec(arg);
+		if (!interact)
+			looper = 0;
 	}
 
 	free(ln);
