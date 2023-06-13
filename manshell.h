52ead2638186a820271a8a37b3277e481d718ed3
equationcode <annypaul80@gmail.com>
2023-06-13 03:44:35 -0700

    builtin.c

--- manshell.h
+++ manshell.h
@@ -15,5 +15,10 @@ char *srch_prt(char *fl);
 void tokenize_command_line(char *ln, char **args);
 void execute_in_child_process(char **args, char **envn, char *prog_nm);
 void exec_cmd(char *ln, char *prog_nm, char **envp);
+int _myhistory(info_t *info);
+int unset_alias(info_t *info, char *str);
+int set_alias(info_t *info, char *str);
+int print_alias(list_t *node);
+int _myalias(info_t *info);
 
 #endif
